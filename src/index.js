5d1e5d4801607f3362b2ec82e196b721e58d5815
Timineri Okoko <timineriokoko@gmail.com>
2024-10-19 20:56:00 +0100

    Starting my pizza app from scratch, hardcoding the index.js file to familiarize with the basics

--- src/index.js
+++ src/index.js
@@ -120,13 +120,27 @@
 //   </React.StrictMode>
 // );
 
-import React from "react";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
+function App() {
+  return <h1> Hello React!</h1>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
